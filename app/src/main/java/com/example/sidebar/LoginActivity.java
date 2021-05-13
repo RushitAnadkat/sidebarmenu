86fb3ebd2b656ca8cb6594c453f2ed97d03b120f
hetul <hetul.d.parmar.20@gmail.com>
2021-05-14 00:40:05 +0530

    I have added one not usable activity so please remove ->activity_loign ->LoginActivity.java

---
+++ app/src/main/java/com/example/sidebar/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.sidebar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toast.makeText(this, "Demo Text", Toast.LENGTH_SHORT).show();
+    }
+}
